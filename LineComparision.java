4f18f253f40a29169dcb8b3df79e15daaae960db
Debasmitasutar <debasmita12.ds@gmail.com>
2023-07-04 02:10:23 +0530

    Added Welcome message for line comparision problem Program

---
+++ LineComparision.java
@@ -0,0 +1,7 @@
+package Lineproblem;
+
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("welcome to line comparision Computation");
+    }
+}
